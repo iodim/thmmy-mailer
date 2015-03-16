ccf39eb12d763b1ac186ff49142a9faf34fbab24
iodim <gorgothian@yahoo.com>
2015-03-16 02:56:17 +0200

    Added multithreading, triggers and callbacks

--- lib/thmmy-notifier.rb
+++ lib/thmmy-notifier.rb
@@ -7,6 +7,7 @@ require 'ruby-prof'
 require 'mechanize'
 require 'digest/md5'
 require 'active_record'
+require 'thread_safe'
 require 'thmmy-notifier/db'
 require 'thmmy-notifier/ethmmy'
 require 'thmmy-notifier/psyche'
@@ -19,6 +20,9 @@ require 'thmmy-notifier/announcement'
 module ThmmyNotifier
 
 	def self.init_ethmmy_agent(username = 'guest', password = 'guest')
+		@@username = username
+		@@password = password
+
 		@ethmmy_agent = Ethmmy.new(username, password)
 		@ethmmy_agent.login
 	end
@@ -43,20 +47,73 @@ module ThmmyNotifier
 		end
 	end
 
-	def self.populate_announcements_table
-		ActiveRecord::Base.transaction do
-			@courses_cache.values.each do |ethmmy_id|
-				@ethmmy_agent.get_all_announcements_by(ethmmy_id).each do |announcement|
-					Announcement.create(announcement.merge(courses_id: @courses_cache.values.index(ethmmy_id) + 1))
+	def self.populate_announcements_table(username, password)
+		threads = []
+		mutex = Mutex.new
+		announcement_hash = {}
+
+		Announcement.initialising = true
+
+		@courses_cache.values.each_slice(@courses_cache.length / 10) do |chunk|
+			threads << Thread.new do
+				Ethmmy.login(username, password) do |ethmmy_agent|
+					chunk.each do |ethmmy_id|
+						announcement_hash[ethmmy_id] = []
+						ethmmy_agent.get_all_announcements_by(ethmmy_id).each do |announcement|
+							mutex.synchronize  do 
+								announcement_hash[ethmmy_id] << announcement
+							end
+						end
+					end
 				end
 			end
 		end
+
+		threads.each do |thread|
+			thread.join
+		end
+
+		ActiveRecord::Base.transaction do 
+			announcement_hash.each do |ethmmy_id, announcements|
+				announcements.each do |announcement|
+					Announcement.create(announcement.merge(courses_id: @courses_cache.values.index(ethmmy_id) +1))
+				end
+			end
+		end
+
+		Announcement.initialising = false
 	end
 
-	def self.check_for_new_announcements
-		@courses_cache.values.each do |ethmmy_id|
-				#Announcement.create(announcement.merge(courses_id: @courses_cache.values.index(ethmmy_id) + 1))
-				pp @ethmmy_agent.get_latest_announcement_by(ethmmy_id)
+	def self.check_for_new_announcements(username, password)
+		threads = []
+		mutex = Mutex.new
+		announcement_hash = {}
+
+		Announcement.initialising = false
+
+		@courses_cache.values.each_slice(@courses_cache.length / 10) do |chunk|
+			threads << Thread.new do
+				Ethmmy.login(username, password) do |ethmmy_agent|
+					chunk.each do |ethmmy_id|
+						mutex.synchronize  do 
+							announcement_hash[ethmmy_id] = ethmmy_agent.get_latest_announcement_by(ethmmy_id)
+						end
+					end
+				end
+			end
 		end
+
+		threads.each do |thread|
+			thread.join
+		end
+
+		ActiveRecord::Base.transaction do 
+			announcement_hash.each do |ethmmy_id, announcement|
+				unless announcement.nil?
+					Announcement.create(announcement.merge(courses_id: @courses_cache.values.index(ethmmy_id) +1))
+				end
+			end
+		end
+
 	end
 end
--- lib/thmmy-notifier/announcement.rb
+++ lib/thmmy-notifier/announcement.rb
@@ -2,5 +2,19 @@ module ThmmyNotifier
 	class Announcement < ActiveRecord::Base
 		validates :uhash, uniqueness: true
 		belongs_to :course
+		after_save :on_new_announcement, unless: :initialising?
+
+		def self.initialising=(state)
+			@@initialising = state
+		end
+
+		protected
+			def on_new_announcement
+				puts self.uhash
+			end
+
+			def initialising?
+				@@initialising
+			end
 	end
 end
